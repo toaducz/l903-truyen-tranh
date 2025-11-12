bfb9904915139e6d7f83b2b105891621cbc9669c
phan duc <phanhuynhtoanduc@gmail.com>
2025-11-12 11:20:57 +0700

    delete

--- api/bookmark/delete.tsx
+++ api/bookmark/delete.tsx
@@ -1,12 +1,12 @@
-import { request } from '@/utils/request'
+// import { request } from '@/utils/request'
 
-export interface DeleteBookmarkResponse {
-  message?: string
-  error?: string
-  status: boolean
-}
+// export interface DeleteBookmarkResponse {
+//   message?: string
+//   error?: string
+//   status: boolean
+// }
 
-export async function deleteApi(slug: string) {
+// export async function deleteApi(slug: string) {
   
-  return await request<DeleteBookmarkResponse>('', '/api/bookmark/delete', 'POST', { slug} )
-}
+//   return await request<DeleteBookmarkResponse>('', '/api/bookmark/delete', 'POST', { slug} )
+// }
